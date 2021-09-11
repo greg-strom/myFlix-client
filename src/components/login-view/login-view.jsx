18d6adaade0393a3640c516b7c8a10a3a0847b0e
gbstrom <85531363+gbstrom@users.noreply.github.com>
2021-09-11 08:27:23 -0400

    imported col & row from react-bootstrap into login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import './login-view.scss';
 
