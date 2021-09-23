76dbcb923679a805c918fbbe172773597cd03e68
gbstrom <85531363+gbstrom@users.noreply.github.com>
2021-09-23 12:49:58 -0400

    made another meaningless change

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
-// Main component (will eventually use all the others)!
+// Main component (will eventually use all the others)!!
 class MyFlixApplication extends React.Component {
   render() {
     return (
